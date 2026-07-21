d1a24164a473ddf653b1c700bab66bc48660eedc
muhammed cansoy <cansoymuhammed@hotmail.com>
2026-07-21 11:39:37 +0300

    Add git submodule add <>

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include "my_math.h"
+#include "math_utils.h"
 
 using namespace std;
 
@@ -6,4 +7,6 @@ int main()
 {
     cout << "here" << endl;
     cout << my_math::add(21, 21) << endl;
+    cout << math_utils::multiply(10, 2) << endl;
+    cout << math_utils::subtract(20, 1) << endl;
 }
