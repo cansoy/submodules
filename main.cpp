59a784b62f4cf9d2ecf8e960b634288420a6c1e8
muhammed cansoy <cansoymuhammed@hotmail.com>
2026-07-22 17:43:55 +0300

    Update used json libary

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include "my_math.h"
 #include "math_utils.h"
 #include <nlohmann/json.hpp>
+#include <vector>
+#include <string>
 
 using namespace std;
 
@@ -10,4 +12,7 @@ int main()
     cout << my_math::add(21, 21) << endl;
     cout << math_utils::multiply(10, 2) << endl;
     cout << math_utils::subtract(20, 1) << endl;
+    std::vector<int> numbers{10, 20, 30, 40};
+    nlohmann::json json_data = numbers;
+    std::cout << json_data << '\n';
 }
